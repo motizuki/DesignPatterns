32388b66ceafc6a600fff17933c6953100531872
motizuki <gustavokm90@gmail.com>
2014-12-31 14:31:47 -0200

    Adding some common classes to illustrates the design patterns.

---
+++ GangOfFour/src/com/edu/common/lab/LockedDoorException.java
@@ -0,0 +1,11 @@
+package com.edu.common.lab;
+
+/**
+ * Created by gustavokm90 on 12/27/14.
+ */
+public class LockedDoorException extends Exception {
+
+    public LockedDoorException(String message) {
+        super(message);
+    }
+}
