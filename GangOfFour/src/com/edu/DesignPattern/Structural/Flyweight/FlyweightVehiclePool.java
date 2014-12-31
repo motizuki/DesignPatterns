30124d43435b1d8afd19748928579329ae11d1e2
motizuki <gustavokm90@gmail.com>
2014-12-31 14:29:59 -0200

    Adding flyweight example of design pattern

---
+++ GangOfFour/src/com/edu/DesignPattern/Structural/Flyweight/FlyweightVehiclePool.java
@@ -0,0 +1,29 @@
+package com.edu.DesignPattern.Structural.Flyweight;
+
+import com.edu.DesignPattern.Structural.Adapter.VehicleClassAdapter;
+import com.edu.common.Products.Car;
+import com.edu.common.Products.Vehicle;
+
+import java.util.HashMap;
+
+/**
+ * Created by gustavokm90 on 12/31/14.
+ */
+public class FlyweightVehiclePool {
+
+    private static final HashMap<String, Vehicle> vehicleMap = new HashMap();
+
+    public static Vehicle getVehicle(String color, String type){
+
+        Vehicle vehicle = vehicleMap.get(color);
+
+        if (vehicle == null){
+            vehicle = new VehicleClassAdapter(type);
+            vehicleMap.put(color,vehicle);
+            System.out.println("Creating Vehicle of type: "+type +" and color: "+color);
+        }else{
+            System.out.println("Using an existent vehicle object");
+        }
+        return vehicle;
+    }
+}
